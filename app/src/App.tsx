8b0895665703dee3c2be3a160819bf4d58c2c9c0
Kota <yuzukh0211@gmail.com>
2023-08-05 19:23:21 +0900

    import title in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import Title from './components/Title';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
 }
 
 export default App;
