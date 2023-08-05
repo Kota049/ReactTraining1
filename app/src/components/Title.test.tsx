96d6799baeb0a56e395d48557344bcd559eb329e
Kota <yuzukh0211@gmail.com>
2023-08-05 19:20:56 +0900

    refactor getting title deal to method

--- app/src/components/Title.test.tsx
+++ app/src/components/Title.test.tsx
@@ -4,9 +4,13 @@ import Title from './Title';
 describe('Title test', () => {
   it('show title', () => {
     render(<Title />);
-    let title = screen.getByRole('heading', { level: 2 });
+    let title = getTitle();
     expect(title.textContent).toBe('今週のプランニング');
   });
 });
+
+const getTitle = (): HTMLElement => {
+  return screen.getByRole('heading', { level: 2 });
+};
 
 export {};
