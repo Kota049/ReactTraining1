83658d95d28b387907634cb935874ba7aa356a8f
Kota <yuzukh0211@gmail.com>
2023-08-05 19:16:48 +0900

    green show title

--- app/src/components/Title.tsx
+++ app/src/components/Title.tsx
@@ -1,3 +1,11 @@
-const Title = () => {};
+import { Typography } from '@mui/material';
+
+const Title = () => {
+  return (
+    <>
+      <Typography variant="h2">今週のプランニング</Typography>
+    </>
+  );
+};
 
 export default Title;
