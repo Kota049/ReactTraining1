cf2264a69595ba55c94f0646e3fc02e04aaed576
Kota <yuzukh0211@gmail.com>
2023-08-05 22:30:20 +0900

    when button click,arg function called

--- app/src/components/Cell.test.tsx
+++ app/src/components/Cell.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import Cell from './Cell';
+import userEvent from '@testing-library/user-event';
 
 describe('Cell', () => {
   it('show button', () => {
     render(<Cell />);
     expect(getButton()).not.toBeNull();
+  });
+  it('when button click,arg function called', async () => {
+    const clickEvent = jest.fn;
+    render(<Cell onClick={clickEvent} />);
+    const button = getButton();
+    await userEvent.click(button);
+
+    expect(clickEvent).toHaveBeenCalled();
   });
 });
 
