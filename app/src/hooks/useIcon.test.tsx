a5e3dbc04907697175219730244e21270b9f5b40
Kota <yuzukh0211@gmail.com>
2023-08-05 23:08:36 +0900

    red when function called, IconStatus become FULL

--- app/src/hooks/useIcon.test.tsx
+++ app/src/hooks/useIcon.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import useIcon from './useIcon';
 
 describe('useIcon for toggle icon', () => {
@@ -6,5 +6,13 @@ describe('useIcon for toggle icon', () => {
     const { result } = renderHook(useIcon);
     expect(result.current[0]).toBe('VACANT');
     expect(typeof result.current[1]).toBe('function');
+  });
+  it('when function called, IconStatus become FULL', () => {
+    const { result } = renderHook(useIcon);
+    const [iconStatus, updateMethod] = result.current;
+    act(() => {
+      updateMethod();
+    });
+    expect(iconStatus).toBe('FULL');
   });
 });
